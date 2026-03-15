decdaed8ddb85e846fdf060c9dcbdbe828707f7a
matixi9 <290216@student.pwr.edu.pl>
2026-03-15 15:24:55 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function bootstrap() {
       transform: true,
       whitelist: true
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cocktailer API')
+    .setDescription('API do zarządzania koktajlami i składnikami (Zadanie Rekrutacyjne)')
+    .setVersion('1.0')
+    .build();
+    
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   
   await app.listen(process.env.PORT ?? 3000);
 }
