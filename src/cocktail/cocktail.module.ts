8d202f8dbb2a31bc45fd60a1d2b333d4ed7cd30d
matixi9 <290216@student.pwr.edu.pl>
2026-03-15 15:31:38 +0100

    added imports to modules

--- src/cocktail/cocktail.module.ts
+++ src/cocktail/cocktail.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CocktailService } from './cocktail.service';
 import { CocktailController } from './cocktail.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cocktail } from './entities/cocktail.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cocktail])],
   controllers: [CocktailController],
   providers: [CocktailService],
 })
--- src/ingredient/ingredient.module.ts
+++ src/ingredient/ingredient.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IngredientService } from './ingredient.service';
 import { IngredientController } from './ingredient.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ingredient } from './entities/ingredient.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ingredient])],
   controllers: [IngredientController],
   providers: [IngredientService],
 })
