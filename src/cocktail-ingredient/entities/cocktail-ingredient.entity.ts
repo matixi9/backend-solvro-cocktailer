cd0526875a4b38ca8c8ec3706c891880c09a7cb8
matixi9 <290216@student.pwr.edu.pl>
2026-03-15 14:06:02 +0100

    added validation to main.ts

--- src/cocktail-ingredient/entities/cocktail-ingredient.entity.ts
+++ src/cocktail-ingredient/entities/cocktail-ingredient.entity.ts
@@ -1,7 +1,6 @@
 import { Cocktail } from "src/cocktail/entities/cocktail.entity";
 import { Ingredient } from "src/ingredient/entities/ingredient.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { ManyToMany } from "typeorm/browser";
 
 @Entity()
 export class CocktailIngredient {
