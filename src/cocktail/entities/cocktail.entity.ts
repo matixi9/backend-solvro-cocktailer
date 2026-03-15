962a0e94b77527b9e4caa2b1e705c58269abdfa6
matixi9 <290216@student.pwr.edu.pl>
2026-03-15 13:47:47 +0100

    fixed entities for TypeORM

--- src/cocktail/entities/cocktail.entity.ts
+++ src/cocktail/entities/cocktail.entity.ts
@@ -1,9 +1,21 @@
+import { CocktailIngredient } from "src/cocktail-ingredient/entities/cocktail-ingredient.entity";
 import { Ingredient } from "src/ingredient/entities/ingredient.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 export class Cocktail {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     category: string;
+
+    @Column()
     instruction: string;
-    ingredients: Ingredient[];
+
+    @OneToMany(() => CocktailIngredient, (cocktailIngredient) => cocktailIngredient.cocktail, {cascade: true})
+    ingredients: CocktailIngredient[];
 }
--- src/ingredient/entities/ingredient.entity.ts
+++ src/ingredient/entities/ingredient.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Ingredient {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @Column()
     isAlcoholic: boolean;
+
+    @Column()
     photo: string;
 }
