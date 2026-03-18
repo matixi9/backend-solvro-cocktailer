41bc1d66ea8e118071b60b9e9240d676ef65fd39
matixi9 <290216@student.pwr.edu.pl>
2026-03-18 13:48:09 +0100

    feat: implement user registration with bcrypt password hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const {email, password} = createUserDto;
+
+    const existingUser = await this.userRepository.findOne({where: {email}});
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists :c');
+    }
+
+    const saltRounds = 10;
+    const hasherPassword = await bcrypt.hash(password, saltRounds);
+  
+    const newUser = this.userRepository.create({
+      email,
+      password: hasherPassword,
+    });
+
+    return await this.userRepository.save(newUser);
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOne({where: {email}});
   }
 }
